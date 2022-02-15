c1f97d7186293cf17315a25bdadf399a570dee6d
Vantol Bennett <vvantol2000@icloud.com>
2022-02-14 23:49:05 -0500

    added Strapi Auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
-<script setup></script>
+<script setup>
+	const route = useRoute()
+	const { find } = useStrapi4()
+
+	try {
+		const response = await find('recipe')
+		console.log(response)
+	} catch (e) {
+		console.log(e)
+	}
+</script>
 <template>
 	<div>
 		<h1>Welcome</h1>
+		<p>Recipes</p>
 	</div>
 </template>
